136024c1fe5970effa11073fabdf7ef662005d80
Andriya Daud Despandi <andriyandaud12@gmail.com>
2024-02-29 04:16:52 +0700

    Homework 3

--- gen-20-java-basic/src/Soal01.java
+++ gen-20-java-basic/src/Soal01.java
@@ -1,6 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Soal01 {
     public static void main(String[] args) {
         int angka = 100;
         String huruf = "Ini adalah angka 100";
---
+++ gen-20-java-basic/src/Soal02.java
@@ -0,0 +1,11 @@
+public class Soal02 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 50;
+
+        int z = x * y /2;
+
+        System.out.println(z);
+
+    }
+}
